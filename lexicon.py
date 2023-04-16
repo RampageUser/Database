3f54e7c24293110d037dd816f781a6f827c99f07
Artur Volnov <fristail95@gmail.com>
2023-04-16 19:46:21 +0500

    add lexicon

---
+++ lexicon.py
@@ -0,0 +1,24 @@
+lexicon_inter: dict = {
+    'Menu': 'Inter number of menu: ',
+    'Correct menu': 'Inter correct number of menu: ',
+    'Name': 'Inter a name: ',
+    'New name': 'Inter a new name: ',
+    'Exist ID': 'Inter en existed ID: ',
+    'Remove': 'Inter a name what you want to remove: ',
+    'Change': 'Inter a name what you want to change: ',
+    'ID': 'Inter ID: ',
+    'ID to change': 'Inter a correct ID of name what you want to change: ',
+}
+
+lexicon_error: dict = {
+    'ID': 'Incorrect ID, please try again later',
+    'Not find': "Didn't found, please try again later",
+    'Value err': 'Incorrect value, please try again later.',
+    'Reference': "You haven't to delete it, because there is a reference",
+}
+
+lexicon_success: dict = {
+    'Created': 'Database was created.',
+    'Added': 'Info has been added.',
+    'Changed': 'Info was changed.',
+}
